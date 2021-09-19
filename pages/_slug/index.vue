afab1df440d02ba6250a5bc077a6a022c89188e7
makoengineer <mako.engineer@gmail.com>
2021-09-19 13:49:02 +0900

    add: ブログ詳細を表示

---
+++ pages/_slug/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $microcms }) {
+
+    const data = await $microcms.get({
+      endpoint: 'blog',
+      contentId: params.slug,
+    });
+    return data;
+  }
+}
+</script>
